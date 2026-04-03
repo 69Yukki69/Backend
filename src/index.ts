88156943543aaad85bbfb35b82ab494ccfda9049
Johnne Ray Teodoro <teodoro.366246@fairview.sti.edu.ph>
2026-04-03 21:52:56 +0800

    fix index

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ const corsOptions = {
     'https://my-app-phi-pearl-24.vercel.app',
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-app.use(cors(corsOptions)); // ← no app.options('*') needed
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
