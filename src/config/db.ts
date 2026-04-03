64828bc4ce61f47440d510ec679032b59f85703c
Johnne Ray Teodoro <teodoro.366246@fairview.sti.edu.ph>
2026-04-03 10:41:22 +0800

    last fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client' 
+import { PrismaClient } from '../generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({
@@ -6,5 +6,4 @@ const adapter = new PrismaPg({
 });
 
 const prisma = new PrismaClient({ adapter });
-
 export default prisma;
--- src/dto/delivery.dto.ts
+++ src/dto/delivery.dto.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import  OrderStatus  from "@prisma/client";
+import { OrderStatus } from '../generated/prisma/client'
 
 export const createDeliverySchema = z.object({
   supplierId: z.string().min(1),
