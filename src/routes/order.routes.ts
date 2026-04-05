0395e7244680bd9396c318f08677ad7980f9374e
Johnne Ray Teodoro <teodoro.366246@fairview.sti.edu.ph>
2026-04-05 20:06:30 +0800

    fix 2

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -7,5 +7,5 @@ import { authMiddleware } from '../middleware/authMiddleware';
 const router = Router();
 
 router.post('/', authMiddleware(['CUSTOMER']), validate(PlaceOrderDto), placeOrder);
-
+ 
 export default router;
