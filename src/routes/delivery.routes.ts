6228526894739aaef45bdbf063b031eeb8066be7
Johnne Ray Teodoro <teodoro.366246@fairview.sti.edu.ph>
2026-04-24 03:16:18 +0800

    delivery routes accessible to stock manager

--- src/routes/delivery.routes.ts
+++ src/routes/delivery.routes.ts
@@ -12,9 +12,9 @@ import { authMiddleware } from "../middleware/authMiddleware";
 const router = Router();
 
 // Create a new delivery
-router.post("/", authMiddleware(["ADMIN"]),createDeliveryController);
-router.get("/", authMiddleware(["ADMIN"]),getAllDeliveriesController);
-router.get("/:id", authMiddleware(["ADMIN"]),getDeliveryByIdController);
+router.post("/", authMiddleware(["ADMIN", "STOCK_MANAGER"]),createDeliveryController);
+router.get("/", authMiddleware(["ADMIN", "STOCK_MANAGER"]),getAllDeliveriesController);
+router.get("/:id", authMiddleware(["ADMIN", "STOCK_MANAGER"]),getDeliveryByIdController);
 router.put("/:id", authMiddleware(["ADMIN"]),updateDeliveryController);
 router.delete("/:id", authMiddleware(["ADMIN"]),deleteDeliveryController);
 router.patch("/:id/receive", authMiddleware(["ADMIN"]),receiveDeliveryController);
